aa58f777414f657cdb6bbc380263e1043ce3736c
DamianaBuono <damianabuono2000@gmail.com>
2024-10-19 16:29:06 +0200

    Fine Navbar

--- src/components/static/Navbar.js
+++ src/components/static/Navbar.js
@@ -1,40 +1,50 @@
-// src/components/static/Navbar.js
-import React from 'react';
+// src/components/Navbar.js
+import React, { useState } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import '../style/Navbar.css'; // Assicurati che il percorso sia corretto
 
 function MyNavbar() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleNavItemClick = () => {
+    setExpanded(false);
+  };
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" className="navbar">
-      <Navbar.Brand href="/">Il Mio Progetto</Navbar.Brand>
+    <Navbar bg="dark" variant="dark" expand="lg" expanded={expanded} onToggle={handleToggle} className="navbar">
+      <Navbar.Brand href="/">Alla scoperta del Giappone</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <LinkContainer to="/">
+          <LinkContainer to="/" onClick={handleNavItemClick}>
             <Nav.Link>Home</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/citta">
+          <LinkContainer to="/citta" onClick={handleNavItemClick}>
             <Nav.Link>Città</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/storia">
+          <LinkContainer to="/storia" onClick={handleNavItemClick}>
             <Nav.Link>Storia</Nav.Link>
           </LinkContainer>
-          <NavDropdown title="Cultura" id="basic-nav-dropdown">
-            <LinkContainer to="/cultura/arti">
+          <NavDropdown title="Cultura" id="basic-nav-dropdown" className="nav-dropdown">
+            <LinkContainer to="/cultura/arti" onClick={handleNavItemClick}>
               <NavDropdown.Item>Arti</NavDropdown.Item>
             </LinkContainer>
-            <LinkContainer to="/cultura/festivita">
+            <LinkContainer to="/cultura/festivita" onClick={handleNavItemClick}>
               <NavDropdown.Item>Festività</NavDropdown.Item>
             </LinkContainer>
-            <LinkContainer to="/cultura/tradizioni">
+            <LinkContainer to="/cultura/tradizioni" onClick={handleNavItemClick}>
               <NavDropdown.Item>Tradizioni</NavDropdown.Item>
             </LinkContainer>
           </NavDropdown>
-          <LinkContainer to="/gastronomia">
+          <LinkContainer to="/gastronomia" onClick={handleNavItemClick}>
             <Nav.Link>Piatti Tipici</Nav.Link>
           </LinkContainer>
-          <LinkContainer to="/posti-da-visitare">
+          <LinkContainer to="/posti-da-visitare" onClick={handleNavItemClick}>
             <Nav.Link>Parchi Nazionali</Nav.Link>
           </LinkContainer>
         </Nav>
